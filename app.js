2ee1af05765db961da5ad01cf370b26ed45b4600
maoruibin001 <www.maoruibin@163.com>
2017-10-24 10:39:00 +0800

    create a front&end project

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 /**
- * Created by lenovo on 2017/10/23.
+ * Created by lenovo on 2017/10/24.
  */
 const express = require('express');
 const path = require('path');
 
-const resolve = function(dir) {
+function resolve(dir) {
   return path.resolve(__dirname, dir);
 }
 
 const app = express();
 
-
-
-app.get('/', function(req, res) {
-  res.sendFile(resolve('./index.html'));
-});
-
-
-app.listen('8084', function() {
-  console.log('server start at:localhost:8084')
+app.use(express.static(resolve('./public')));
+app.listen(8787, function() {
+  console.log('server start at: localhost:8787');
 });
